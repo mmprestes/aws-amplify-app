dcf67d64af3d950eec6154ffd53080870911a2b2
Marcelo Prestes <marcelo.prestes@gmail.com>
2022-01-15 23:07:19 -0300

    Link to mobisoft.com.br added to page botton

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://mobisoft.com.br"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Acesse mobisoft.com.br
         </a>
       </header>
     </div>
